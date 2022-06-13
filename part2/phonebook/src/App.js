fd503aed49031f1575480680023934fab582fb79
H4ni0 <aboahmedhani82@gmail.com>
2022-06-13 12:09:57 +0300

    part2 finished

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,8 @@
 import { useState, useEffect } from 'react'
-// import _ from 'lodash'
-import axios from 'axios'
+import personService from './services/persons'
 
-const Person = ({name, number}) => {
-  return <div>{name} {number}</div>
+const Person = ({person, removePerson}) => {
+  return <div>{person.name} {person.number} <button onClick={() => removePerson(person.id)}>delete</button></div>
 }
 
 const Filter = ({search, setSearch}) => <div>filter shown with <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}/></div> 
@@ -22,16 +21,28 @@ const PersonForm = ({add, newName, setNewName, newNumber, setNewNumber}) => {
 )
 }
 
-const Persons = ({persons, search}) => persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase())).map(person => <Person key={person.id} name={person.name} number={person.number} />)
+const Persons = ({persons, search, removePerson}) => persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase())).map(person => <Person removePerson={removePerson} key={person.id} person={person}/>)
+
+const Alert = ({ alert }) => {
+  if (alert) return <div className='alert'>{alert}</div>
+  else return
+}
+
+const RedAlert = ({redAlert}) => {
+  if (redAlert) return <div className='redAlert'>{redAlert}</div>
+  else return
+}
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+  const [alert, setAlert] = useState('')
+  const [redAlert, setRedAlert] = useState('')
 
   const hook = () => {
-    axios.get("http://localhost:3001/persons").then(response => setPersons(response.data))
+    personService.getAll().then(allPersons => setPersons(allPersons))
   }
   useEffect(hook, [])
 
@@ -40,27 +51,52 @@ const App = () => {
     const newPerosn = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1
     }
 
     if(!persons.some(person => person.name.trim() === newPerosn.name.trim())) {
-    setPersons(persons.concat(newPerosn))
-    setNewName("")
-    setNewNumber("")
+      personService.create(newPerosn).then(createdPerson => setPersons(persons.concat(createdPerson)))
+      setNewName("")
+      setNewNumber("")
+      setAlert("Added " + newPerosn.name)
+      setTimeout(() => {
+        setAlert("")
+      }, 3000);
     } else {
-      alert(`${newName} is already added to phonebook`)
+      if (window.confirm(`${newName} is already added to phonebook, replace old number with a new one?`)) {
+        personService.update(persons.find(person => person.name.trim() === newPerosn.name.trim()).id, newPerosn).then( res =>
+          setPersons(persons.map(person => person.name.trim() !== newPerosn.name.trim() ? person : res))
+        ).catch(err => {
+          setRedAlert("Information of " + newName + " has already been removed from server")
+        })
+        setNewName("")
+        setNewNumber("")
+      }
     }
 
+  }
+
+
+  const removePerson = (id) => {
+    if(window.confirm(`delete ${persons.find(person => person.id === id).name}`)) personService
+      .remove(id)
+      .then(setPersons(persons.filter((person) => person.id !== id)))
+      .catch((err) => {
+        setRedAlert(
+          "This user has already been removed from server"
+        );
+      });
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Alert alert={alert}/>
+      <RedAlert redAlert={redAlert} />
       <Filter search={search} setSearch={setSearch} />
       <h2>Add a new</h2>
       <PersonForm add={add} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} />
       <h2>Numbers</h2>
-      <Persons persons={persons} search={search} />
+      <Persons removePerson={removePerson} persons={persons} search={search} />
     </div>
   )
 }
---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+const baseUrl = "http://localhost:3001/persons"
+
+const getAll = () => axios.get(baseUrl).then(res => res.data)
+const create = obj => axios.post(baseUrl, obj).then(res => res.data)
+const update = (id, obj) => axios.put(`${baseUrl}/${id}`, obj).then(res => res.data)
+const remove = (id) => axios.delete(`${baseUrl}/${id}`)
+
+const exp = {getAll, create, update, remove}
+export default exp
