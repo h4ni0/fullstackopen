4bcaa167b6a41ed215bf8577ecce43b0a18da4c2
H4ni0 <aboahmedhani82@gmail.com>
2022-07-02 12:10:03 +0300

    part7.b finished

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,8 +1,8 @@
 import {
-    BrowserRouter as Router,
     Routes, Route, Link, useMatch, useNavigate
 } from 'react-router-dom'
 import { useState } from 'react'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -53,17 +53,18 @@ const Footer = () => (
 
 const CreateNew = (props) => {
     const navigate = useNavigate()
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+
+    const [content, resetContent] = useField('text')
+    const [author, resetAuthor] = useField('text')
+    const [info, resetInfo] = useField('text')
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: content.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
@@ -75,19 +76,20 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input { ...content } />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input { ...author } />
         </div>
-        <div>
-          url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+            <div>
+                url for more info
+                <input { ...info } />
+            </div>
+            <button>create</button>
+            </form>
+            <button onClick={() => {resetContent();resetAuthor();resetInfo()}}>reset</button>
         </div>
-        <button>create</button>
-      </form>
-    </div>
   )
 
 }
---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+    
+    const onChange = (e) => {
+        e.preventDefault()
+        setValue(e.target.value)
+    }
+
+    const reset = () => {
+        setValue('')
+    }
+
+    return [{type, value, onChange}, reset]
+}
